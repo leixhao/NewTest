b20c02c0f33af655e9660c7492b3afd09152983c
leixhao <982629377@qq.com>
2020-05-29 17:45:16 +0800

    信息更新监听和发布

--- 响应式原理.js
+++ 响应式原理.js
@@ -1,19 +1,48 @@
-function cb(val){
-  console.log(val)
-  console.log('视图更新了')
+// function cb(val){
+//   console.log(val)
+//   console.log('视图更新了')
+// }
+
+class Dep{
+  constructor(){
+    this.subs = []
+  }
+  addSub(sub){
+    this.subs.push(sub)
+  }
+  notify(){
+    this.subs.forEach((sub) => {
+      sub.update()
+    })
+  }
+}
+
+class Watcher{
+  constructor(){
+    Dep.target = this
+  }
+
+  update(){
+    console.log('视图已经更新')
+  }
 }
 
 function defineReactive(obj, key, val){
+  // 创建一个订阅者
+  const dep = new Dep()
   Object.defineProperty(obj, key, {
     enumerable: true, //设置属性可被枚举
     configurable: true, //设置属性可被修改和删除
     get:function reactiveGetter(){
+      // 为每一个获取该属性的对象 添加依赖
+      dep.addSub(Dep.target)
       return val
     },
     set: function reactiveSetter(newVal){
       if( newVal === val ) return
       val = newVal
-      cb(newVal)
+      // cb(newVal)
+      dep.notify()
     }
   })
 }
@@ -31,6 +60,9 @@ class Vue{
   constructor(options){
     this._data = options.data;
     observer(this._data)
+    // 新建一个watcher观察者对象，这个时候观察者中的Dep会指向这个Watcher对象
+    new Watcher()
+    console.log('render~',this._data.count)
   }
 }
 
